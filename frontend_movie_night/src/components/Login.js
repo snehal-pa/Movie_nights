6cc2f30b8bbe8195006eabb2623053056e64296b
alebal14 <alexandra.b.balogh@gmail.com>
2021-01-28 08:28:30 +0100

    login

--- frontend_movie_night/src/components/Login.js
+++ frontend_movie_night/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import GoogleLogin from "react-google-login"
 
 
 import {
@@ -7,17 +8,37 @@ import {
   Row
 } from "reactstrap";
 
+const CLIENT_ID = "58233015853-ebr03ggbna9ohtlisggmftjsqpnsnsf0.apps.googleusercontent.com";
 
 export default function Login() {
+
+  const handleLogin = async googleData => {
+    const res = await fetch("/storeauthcode", {
+        method: "POST",
+        body: JSON.stringify({
+        token: googleData.tokenId
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const data = await res.json()
+    // store returned user somehow
+  }
    
 
-  return (
-    
-    
+  return (       
     <Container className="mt-5">
       <Row className="justify-content-center">
-        <Button className="signinButton">Link my Google Calendar</Button>
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Log in with Google"
+          onSuccess={handleLogin}
+          onFailure={handleLogin}
+          
+        />
       </Row>
     </Container>
   )
+
 }
