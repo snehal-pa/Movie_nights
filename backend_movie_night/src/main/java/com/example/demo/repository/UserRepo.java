cb8c12a4b402188275bf0bc3629f731d75f65b41
anna <zoora1234@hotmail.com>
2021-01-28 21:11:44 +0100

    check if user exist in db
    only save user to db when they login with google account if they dont already exists

--- backend_movie_night/src/main/java/com/example/demo/repository/UserRepo.java
+++ backend_movie_night/src/main/java/com/example/demo/repository/UserRepo.java
@@ -5,5 +5,6 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface UserRepo extends Neo4jRepository<User,Long> {
 
-
+    User findByEmail(String email);
 }
+
--- backend_movie_night/src/main/java/com/example/demo/services/UserService.java
+++ backend_movie_night/src/main/java/com/example/demo/services/UserService.java
@@ -2,8 +2,11 @@ package com.example.demo.services;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,7 +15,12 @@ public class UserService {
 
 
     public void registerUser(User user) {
-        userRepo.save(user);
+        //Are user in database?
+        Optional<User> optional = Optional.ofNullable(userRepo.findByEmail(user.getEmail()));
+
+        if(!optional.isPresent()){
+            userRepo.save(user);
+        }
 
     }
 }
