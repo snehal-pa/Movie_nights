242520694123d20ddb3c95fe4aa164b618488e1f
snehal patel <snehal.kathiriya@gmail.com>
2021-01-28 12:30:11 +0100

    removed button error

--- frontend_movie_night/src/components/Login.js
+++ frontend_movie_night/src/components/Login.js
@@ -1,58 +1,50 @@
-import React, {useEffect} from "react";
-import GoogleLogin from "react-google-login"
+import React, { useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { Button } from "reactstrap";
 
+import { Container, Row } from "reactstrap";
 
-import {  
-  Container,  
-  Row
-} from "reactstrap";
-
-const CLIENT_ID = "58233015853-ebr03ggbna9ohtlisggmftjsqpnsnsf0.apps.googleusercontent.com";
+const CLIENT_ID =
+  "58233015853-ebr03ggbna9ohtlisggmftjsqpnsnsf0.apps.googleusercontent.com";
 
 export default function Login() {
+  let auth2;
 
-  let auth2
-
-  useEffect(()=> {
-    window.gapi.load('auth2', function() {
+  useEffect(() => {
+    window.gapi.load("auth2", function () {
       auth2 = window.gapi.auth2.init({
         client_id: CLIENT_ID,
-        scope: "https://www.googleapis.com/auth/calendar.events"
+        scope: "https://www.googleapis.com/auth/calendar.events",
       });
     });
-  },[])
+  }, []);
 
-  
-async function signInCallback(authResult) {
-  console.log('authResult', authResult);
+  async function signInCallback(authResult) {
+    console.log("authResult", authResult);
 
-  if (authResult['code']) {
-
-    // Send the code to the server
-    let result = await fetch('http://localhost:8080/storeauthcode', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/octet-stream; charset=utf-8',
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-      body: authResult['code']
-    });
-    // etc...
-  } else {
-    // There was an error.
+    if (authResult["code"]) {
+      // Send the code to the server
+      let result = await fetch("http://localhost:8080/storeauthcode", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/octet-stream; charset=utf-8",
+          "X-Requested-With": "XMLHttpRequest",
+        },
+        body: authResult["code"],
+      });
+      // etc...
+    } else {
+      // There was an error.
+    }
   }
-}
-   
 
-  return (       
+  return (
     <Container className="mt-5">
       <Row className="justify-content-center">
-       <Button onClick={()=>  auth2.grantOfflineAccess().then(signInCallback)} >
+        <Button onClick={() => auth2.grantOfflineAccess().then(signInCallback)}>
           Login with Google
-       </Button>
-
+        </Button>
       </Row>
     </Container>
-  )
-
+  );
 }
