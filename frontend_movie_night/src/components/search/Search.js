3856627f464d202aa0c1a408939215c5226a9e3c
alebal14 <alexandra.b.balogh@gmail.com>
2021-02-01 09:27:36 +0100

    wip

--- frontend_movie_night/src/components/search/Search.js
+++ frontend_movie_night/src/components/search/Search.js
@@ -8,11 +8,7 @@ export default function Search() {
   const[searchTerm, setSearchTerm] = useState('');
 
     return (
-      <Container className="container-search mt-4">
-        <Row>
-          <Col lg="8" md="12" sm="12">
-          </Col>
-          <Col lg="4 " md="12" sm="12">
+      <Container className="container-search mt-4">        
           <Row >
           <Col lg="12" md="12" sm="12" >
             <InputGroup>
@@ -24,7 +20,7 @@ export default function Search() {
           <Col lg="6" md="12" sm="12">
             <Media>
               <Media left middle href="#">
-              <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+              <img  className="movie-poster" src="https://images-na.ssl-images-amazon.com/images/I/51JaPkQ-R-L._AC_.jpg" alt="Generic placeholder image" />
             </Media>
             </Media>
           </Col>
@@ -41,12 +37,9 @@ export default function Search() {
             <Media className="p-1 pl-2 pb-2">
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
             </Media>
-            </Col>
-        
+            </Col>      
             
-          </Row>
-        </Row>
-          </Col>
+          </Row>        
         </Row>
         
       </Container>
