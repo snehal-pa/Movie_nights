8eef2ee355fb948ae6e0479a5a4292c429908f03
alebal14 <alexandra.b.balogh@gmail.com>
2021-02-01 07:56:42 +0100

    topbar

---
+++ frontend_movie_night/src/components/TopBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { NavItem, NavLink, Nav, Badge, Navbar, NavbarBrand } from "reactstrap";
+import { NavLink as RRNavLink, Link } from "react-router-dom";
+
+export default function Topbar(){
+
+    return(
+        <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">MovieNights</NavbarBrand>        
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+              <NavLink tag={Link} to={"/"}>               
+                  Login
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to={"/home"}>               
+                  Home
+                </NavLink>              
+              </NavItem>   
+             
+              <NavItem>
+                <NavLink tag={Link} to={"/"}>  
+                <Badge className="mr-2">0</Badge>             
+                  Invitations
+                </NavLink>              
+              </NavItem>
+
+              <NavItem>
+                <NavLink tag={Link} to={"/"}>               
+                  UserName
+                </NavLink>              
+              </NavItem>
+              
+              <NavItem>
+                <NavLink tag={Link} to={"/"}>               
+                  Logout
+                </NavLink>              
+              </NavItem>         
+            </Nav>           
+        </Navbar>
+      </div>  
+    );
+}
