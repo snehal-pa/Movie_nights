1d89d93b3b3282d90dce8e303fe860ba5847bd3d
anna <zoora1234@hotmail.com>
2021-02-01 20:45:50 +0100

    availabletime done
    availabletime done

--- backend_movie_night/src/main/java/com/example/demo/controllers/EventController.java
+++ backend_movie_night/src/main/java/com/example/demo/controllers/EventController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.User;
 import com.example.demo.services.EventService;
 
 
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import com.google.api.client.util.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,11 +15,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 import java.util.List;
 import java.util.TimeZone;
+
+import static java.time.format.DateTimeFormatter.BASIC_ISO_DATE;
+import static java.time.temporal.WeekFields.ISO;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -28,8 +33,10 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
+    //  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+
     @GetMapping("/availablefriends")
-    public ResponseEntity getAvailableFriends(@RequestParam(value="startdate") DateTime startDate, @RequestParam(value="enddate") DateTime endDate){
+    public ResponseEntity getAvailableFriends(@RequestParam(value="startdate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") Date startDate, @RequestParam(value="enddate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")Date endDate){
 
 
         System.out.println("DATE " + startDate);
