fb6deb85cc9c8edd0b7c9d6c130f829e12c1e91f
alebal14 <alexandra.b.balogh@gmail.com>
2021-02-02 11:02:13 +0100

    get all movie

--- frontend_movie_night/src/components/search/Search.js
+++ frontend_movie_night/src/components/search/Search.js
@@ -1,4 +1,4 @@
-import { Container, InputGroup, Input, Media, Col, Row, Button, Form, Label, FormGroup } from "reactstrap";
+import { Container, InputGroup, Input, Media, Col, Row} from "reactstrap";
 import {useState, useEffect} from 'react';
 
 
@@ -7,16 +7,22 @@ export default function Search() {
 
   const[searchTerm, setSearchTerm] = useState('');
 
+  const[ allMovies, setAllMovies] = useState([])
+
   async function fetchAllMovies(){
     let movies = await(
       await fetch("http://localhost:8080/rest/movies")
     ).json();
     console.log(movies)
+    if (movies.error) {
+      movies = [];
+    }
+    setAllMovies(movies);
   }
 
   useEffect(() => {
     fetchAllMovies();
-  });
+  }, []);
 
     return (
       <Container className="container-search mt-4">        
@@ -27,61 +33,41 @@ export default function Search() {
             </InputGroup>
           </Col>
         </Row>       
-        <div className="movielist-box">
-          <Row className="media-item">
+        
+        {allMovies.map((movie) => 
+          (
+          <div className="movielist-box">
+            <Row className="media-item" key={movie.id}>
             <Col lg="3" md="3" sm="12">
               <Media>
                 <Media left middle href="#">
-                <img  className="movie-poster" src="https://images-na.ssl-images-amazon.com/images/I/51JaPkQ-R-L._AC_.jpg" alt="Generic placeholder image" />
+                <img  className="movie-poster" src={`https://image.tmdb.org/t/p/original/${movie.postPath}`} alt="Generic placeholder image" />
               </Media>
               </Media>
             </Col>
             <Col lg="9" md="9" sm="12">
               <Row>
                 <Media body>
-                  <Media heading className="media-heading">Title</Media>          
+                  <Media heading className="media-heading">{movie.title}</Media>          
                 </Media>
               </Row>
               <Row>
                 <Media>
-                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+                {movie.description}
                 </Media>
               </Row>          
-            </Col>                   
-          </Row>  
-          <Row>
-            <Col lg="12">
-              <hr></hr>
-            </Col>
-          </Row> 
-
-          <Row className="media-item">
-            <Col lg="3" md="3" sm="12">
-              <Media>
-                <Media left middle href="#">
-                <img  className="movie-poster" src="https://i.pinimg.com/564x/46/da/e5/46dae512e375bee2664a025507da8795.jpg" alt="Generic placeholder image" />
-              </Media>
-              </Media>
-            </Col>
-            <Col lg="9" md="9" sm="12">
-              <Row>
-                <Media body>
-                  <Media heading className="media-heading">Title</Media>          
-                </Media>
-              </Row>
-              <Row>
-                <Media>
-                  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-                </Media>
-              </Row>          
-            </Col>                   
+            </Col>                              
           </Row>  
           <Row>
             <Col lg="12">
               <hr></hr>
             </Col>
           </Row>           
-        </div>    
+        </div>           
+          )
+          )}
+         
+            
       </Container>
     );
   }
