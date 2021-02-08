1db7293a409f8eaf108d6f5d76d145652edc63b9
alebal14 <alexandra.b.balogh@gmail.com>
2021-02-08 08:29:55 +0100

    wip

--- frontend_movie_night/src/components/CreateInvitation.js
+++ frontend_movie_night/src/components/CreateInvitation.js
@@ -4,7 +4,7 @@ import React, {useContext, useState} from 'react';
 import moment from "moment";
 import Select from "react-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faSearch } from "@fortawesome/free-solid-svg-icons";
+import {  faSearch, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -100,8 +100,8 @@ export default function CreateInvitation(props) {
                   <CardBody className="text-center"> 
                   <Row><img className="avatar" src={`${props.sendMovie.postPath}`} onError={(e) => (e.target.onError = null, e.target.src = 'https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_400,h_264/https://psykologisk-metod.se/wp-content/themes/unbound/images/No-Image-Found-400x264.png')} ></img>  </Row>     
                   <Row className="off-row">
-                    <Col lg="2"></Col>
-                    <Col lg="6"><CardTitle className="movie-title text-left">{props.sendMovie.title}</CardTitle></Col>
+                    <Col lg="3"></Col>
+                    <Col lg="5"><CardTitle className="movie-title text-left">{props.sendMovie.title}</CardTitle></Col>
                     <Col lg="4">
                       <Row className="justify-content-end pb-1"><CardSubtitle className="movie-genre text-muted">{props.sendMovie.genre[0]}</CardSubtitle></Row>
                       <Row className="justify-content-end"><CardSubtitle className="movie-releasedate text-muted">{props.sendMovie.releaseDate}</CardSubtitle></Row>
@@ -153,11 +153,52 @@ export default function CreateInvitation(props) {
                     <Label for="selectFriends">Invite your Friends</Label>                  
                     <Select  options={friends} onChange={handleInvites} isMulti/>             
                   </FormGroup>
-                </Col> ) : null}
+                </Col> ) : null}               
+              </Row>
+                    
                
+              <Row>
+                <Col lg="12">
+                  <hr></hr>
+                </Col>
+              </Row> 
+
+              <Row>
+                <Col lg="12">
+                <div class="cardWrap mr-auto">
+                  <div class="card-ticket cardLeft">
+                    <h1>{context.loggedInUser.name + "'s Movie Night"}</h1>
+                    <div class="title">
+                      <h2>{props.sendMovie.title}</h2>
+                      <span>movie</span>
+                    </div>  
+                    <Row>
+                      <Col lg="6">
+                        <div class="time">
+                          <h2>{moment(combStartDate).format("YYYY-MM-DD HH:mm")}</h2>
+                          <span>start time</span>
+                        </div>
+                      </Col>
+                      <Col lg="6">
+                        <div class="time">
+                          <h2>{moment(combEndDateTime).format("YYYY-MM-DD HH:mm")}</h2>
+                          <span>end time</span>
+                        </div>
+                      </Col>
+                    </Row>
+                    </div>
+                    <div class="card-ticket cardRight">
+                    <div className="d-flex justify-content-center"><FontAwesomeIcon icon={faUserFriends} /></div>
+                      <div class="number">
+                        <h3>{invitesList.length}</h3>
+                        <span>attendant</span>
+                      </div>                      
+                  </div>
+                </div>  
+                </Col>
               </Row>
 
-              <Row>       
+              <Row className="mt-5">       
                 <Container className="vbottom"> 
                   <Row >
                     <Col lg="12">
